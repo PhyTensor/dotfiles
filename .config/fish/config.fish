2801743e8034b55aeb6eda3ccbbaa024d81cc90c
Phystro <phystroq@gmail.com>
2024-12-22 12:45:06 +0300

    yazi shell wrapper

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -127,3 +127,14 @@ end
 
 set MANPAGER "nvim +Man!"
 
+# YAZI
+# Shell wrapper for yazi. We suggest using this y shell wrapper that provides the ability to change the current working directory when exiting Yazi.
+function y
+	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	yazi $argv --cwd-file="$tmp"
+	if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+		builtin cd -- "$cwd"
+	end
+	rm -f -- "$tmp"
+end
+
